258a6b8caab7e76609fd74f81f17b4b3565ede08
Сазонов Егор <diobrando28@gmail.com>
2023-04-27 21:59:32 +0500

    Первый запуск с костылями

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+
+from jirnich import create_app
+
+
+app = create_app()
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+if __name__ == '__main__':
+    #Manager из flask-script не работает, ебанул напрямик, костыль.
+    app.run()
