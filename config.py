f856acd540f9697d59f35d94bd70cb8429756857
Глеб Батог <gora_2000@mail.ru>
2023-04-29 15:08:53 +0500

    add bd

--- config.py
+++ config.py
@@ -5,11 +5,13 @@ class Config(object):
     DEBUG = True
     CSRF_ENABLED = True
     SECRET_KEY = 'YOUR_RANDOM_SECRET_KEY'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost:5432/db_name'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123@localhost:5432/db_name')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProductionConfig(Config):
     DEBUG = False
+
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from jirnich import create_app
 
 
 app = create_app()
-app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 if __name__ == '__main__':
     #Manager из flask-script не работает, ебанул напрямик, костыль.
